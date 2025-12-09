9fc24f654268cfbb53b5363f8cc5b3d127c9dbee
beedawn <brandonschmersal@gmail.com>
2025-12-08 20:29:43 -0500

    day3

--- day3/day3_1.py
+++ day3/day3_1.py
@@ -1,12 +1,4 @@
 import csv
-from unicodedata import digit
-
-
-class DigitPosition:
-    def __init__(self,digit, index, row):
-        self.row = row
-        self.digit = digit
-        self.index = index
 
 def process_file():
     # Open the CSV file in read mode ('r')
@@ -20,45 +12,62 @@ def process_file():
                 arr.append(item)
     return arr
 
-def find_largest(s, row):
-    largest = 0
-    index = 0
-    if len(s) == 0:
-        return 0
-    else:
-        for x in range(len(s)):
-            if s[x].isdigit() and int(s[x]) > largest:
-                largest = int(s[x])
-                index = x
-    return DigitPosition(largest,index, row)
+
+def best_two_digit_number(s):
+    best = -1
+    n = len(s)
+    for i in range(n):
+        for j in range(i + 1, n):
+            num = int(s[i] + s[j])
+            if num > best:
+                best = num
+    return best
+
+
+def best_12_digit_number(s):
+    result = []
+    start = 0
+    n = len(s)
+
+    for pos in range(12):
+        remaining = 12 - pos - 1
+        end = n - remaining
+        best_digit = max(s[start:end])
+        best_index = s.index(best_digit, start, end)
+        result.append(best_digit)
+        start = best_index + 1
+
+    return "".join(result)
+
 
 def step_one(arr):
     result = []
     sum_strs = []
     for row, item in enumerate(arr):
-        for i in range(2):
-            digitItem = find_largest(item, row)
-            result.append(digitItem)
-            #need to change this to, once a digit is chosen, we need to pick whatever is to the right of it?
-            # if nothing is to the right of it then we need to get the next largest digit?
-            item = item[0:digitItem.index]+item[digitItem.index+1:]
-    sum = 0
-    for index, item in enumerate(result):
-        print(item.digit, item.index, item.row)
-        if index+1 < len(result) and item.row == result[index+1].row:
-            if item.index > result[index+1].index:
-                str_num = str(result[index+1].digit)+str(item.digit)
-                print(str_num)
-                sum_strs.append(str_num)
-            else:
-                str_num = str(item.digit)+ str(result[index+1].digit)
-                print(str_num)
-                sum_strs.append(str_num)
-    for num in sum_strs:
-        sum += int(num)
-    print(sum)
+            number = best_two_digit_number(item)
+            sum_strs.append(number)
+    sum1 = 0
+    for item in sum_strs:
+        sum1 += int(item)
+    print(sum1)
+
+
+def step_two(arr):
+    result = []
+    sum_strs = []
+    for row, item in enumerate(arr):
+            number = best_12_digit_number(item)
+            sum_strs.append(number)
+    sum1 = 0
+    for item in sum_strs:
+        sum1 += int(item)
+    print(sum1)
+
+
 
 if __name__ == "__main__":
     arr = process_file()
 
     step_one(arr)
+
+    step_two(arr)
